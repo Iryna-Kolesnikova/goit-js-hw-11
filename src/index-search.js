b1293e0b8f2f7109a1c2e1d3965a707cb69a6c3a
Iryna-Kolesnikova <125210912+Iryna-Kolesnikova@users.noreply.github.com>
2023-07-25 21:25:22 +0200

    next changes

--- src/index-search.js
+++ src/index-search.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import 'notiflix'; 
-import SimpleLightbox from 'simplelightbox';
 import { Report } from 'notiflix/build/notiflix-report-aio';
+import SimpleLightbox from 'simplelightbox';
 
 const apiKey = '38421129-385ad5ee77a37193a2d5ae11d'; 
 const perPage = 40;
@@ -10,6 +10,64 @@ let currentPage = 1;
 const searchForm = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
+
+function showLoadMoreButton() {
+  loadMoreBtn.style.display = 'block';
+}
+
+function hideLoadMoreButton() {
+  loadMoreBtn.style.display = 'none';
+}
+
+function showEndOfResultsMessage() {
+  Notiflix.Report.info("Info", "You've reached the end of search results.", "OK");
+}
+
+async function handleSearch(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const searchQuery = formData.get('searchQuery');
+
+  if (!searchQuery) {
+    return;
+  }
+
+  try {
+    currentPage = 1;
+    gallery.innerHTML = '';
+    const images = await fetchImages(searchQuery);
+    if (images.totalHits === 0) {
+      Notiflix.Report.failure("Oops!", "Sorry, there are no images matching your search query. Please try again.", "OK");
+      hideLoadMoreButton(); 
+    } else {
+      displayImages(images.hits);
+      if (images.totalHits <= perPage) {
+        hideLoadMoreButton(); 
+      } else {
+        showLoadMoreButton(); 
+      }
+    }
+  } catch (error) {
+    Notiflix.Report.failure("Error", error.message, "OK");
+    hideLoadMoreButton();
+  }
+}
+
+async function handleLoadMore() {
+  try {
+    currentPage++;
+    const searchQuery = document.querySelector('input[name="searchQuery"]').value;
+    const images = await fetchImages(searchQuery);
+    displayImages(images.hits);
+    if (images.totalHits <= currentPage * perPage) {
+      hideLoadMoreButton(); 
+      showEndOfResultsMessage(); 
+    }
+  } catch (error) {
+    Notiflix.Report.failure("Error", error.message, "OK");
+    hideLoadMoreButton(); 
+  }
+}
 
 async function fetchImages(query) {
   try {
@@ -48,6 +106,9 @@ function displayImages(images) {
     `;
     gallery.appendChild(card);
   });
+
+
+  lightbox.refresh();
 }
 
 async function handleSearch(event) {
@@ -91,6 +152,5 @@ async function handleLoadMore() {
 
 searchForm.addEventListener('submit', handleSearch);
 loadMoreBtn.addEventListener('click', handleLoadMore);
-
 
 const lightbox = new SimpleLightbox('.gallery__item');
